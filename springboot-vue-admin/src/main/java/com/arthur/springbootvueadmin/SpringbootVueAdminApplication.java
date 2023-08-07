577928da9ea71ccff563e65d436657888849cf2b
arthurzengg <arthurzeng@berkeley.edu>
2023-08-07 14:46:21 +0800

    create a new module

---
+++ springboot-vue-admin/src/main/java/com/arthur/springbootvueadmin/SpringbootVueAdminApplication.java
@@ -0,0 +1,13 @@
+package com.arthur.springbootvueadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootVueAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootVueAdminApplication.class, args);
+	}
+
+}
---
+++ springboot-vue-admin/src/test/java/com/arthur/springbootvueadmin/SpringbootVueAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arthur.springbootvueadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootVueAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
