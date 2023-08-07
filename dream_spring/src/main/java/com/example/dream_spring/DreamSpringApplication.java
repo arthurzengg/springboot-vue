8c867491ae61013107a9ac4381b9044a6d68f5dd
arthurzengg <arthurzeng@berkeley.edu>
2023-08-07 15:28:38 +0800

    Create Dream Spring Project

---
+++ dream_spring/src/main/java/com/example/dream_spring/DreamSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.dream_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DreamSpringApplication.class, args);
+    }
+
+}
---
+++ dream_spring/src/test/java/com/example/dream_spring/DreamSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dream_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
